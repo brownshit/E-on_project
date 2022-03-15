a70b07c9091eafdf762cc14db97038f56c5a713b
원종찬 <100653363+brownshit@users.noreply.github.com>
2022-03-16 00:50:37 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+	3주차 이온과제
+	- 입력된 6개 수의 합을 구하는 문제
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int arr_sum(int *ptr);
+
+int main(void)
+{
+	int numarr[6] = { 0 };
+	
+	for (int i = 0; i < 6; i++)
+		scanf("%d",&numarr[i]);
+	
+	printf("\n%d\n", arr_sum(numarr));
+
+	return 0;
+}
+
+int arr_sum(int *ptr)
+{
+	int sum_in_fun = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		sum_in_fun += ptr[i];
+	}
+
+	return sum_in_fun;
+}
